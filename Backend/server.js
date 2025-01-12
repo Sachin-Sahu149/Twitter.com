0a6d0de0516dc75edce51884bbda28fb3a80b393
Sachin-Sahu149 <sachinsahu3222@gmail.com>
2025-01-12 13:29:07 +0530

    Correct spelling mismatch

--- Backend/server.js
+++ Backend/server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
 
 if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static(path.join(_dirname,'/Frotend/dist')));
+    app.use(express.static(path.join(_dirname,'/Frontend/dist')));
 
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(_dirname,'Frontend','dist','index.html'));
